0e45da2e408367621a4c11857e80c9a5f3e64722
Caio Alves <caioanegocios@gmail.com>
2026-03-08 17:43:41 -0300

    Create AccountClientController

---
+++ src/main/java/com/bank_service/client/controller/AccountClientController.java
@@ -0,0 +1,42 @@
+package com.bank_service.client.controller;
+
+import com.bank_service.client.dto.AccountDTO;
+import com.bank_service.client.dto.OperationDTO;
+import com.bank_service.client.dto.TransferDTO;
+import com.bank_service.client.service.AccountClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping(value = "/account")
+public class AccountClientController {
+
+    @Autowired
+    private AccountClientService accountClientService;
+
+    @GetMapping(value = "/{id}")
+    private ResponseEntity<AccountDTO> getAccount(@PathVariable UUID id) {
+        return ResponseEntity.ok(accountClientService.getAccountById(id));
+    }
+
+    @PatchMapping(value = "/{id}/deposit")
+    private ResponseEntity<AccountDTO> deposit(@PathVariable UUID id, @RequestBody OperationDTO operation) {
+        return ResponseEntity.ok(accountClientService.deposit(id, operation));
+    }
+
+    @PatchMapping(value = "/{id}/withdraw")
+    public ResponseEntity<Object> withdraw(@PathVariable UUID id, @RequestBody OperationDTO operation) {
+        Object response = accountClientService.withdraw(id, operation);
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping(value = "/transfer")
+    public ResponseEntity<Object> transfer(@RequestBody TransferDTO transfer) {
+        Object response = accountClientService
+                .transfer(transfer.getAccountSender(), transfer);
+        return ResponseEntity.ok(response);
+    }
+}
